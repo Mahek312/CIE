bd4a8e7dedbb1be5ad28f238a4ae2fcfcf6ee647
Mahek312 <Mahekrehamanavar@gmail.com>
2025-04-04 10:48:20 +0530

    CIE program

---
+++ basiccalculator.java
@@ -0,0 +1,13 @@
+package CIE;
+
+public class basiccalculator {
+    static float a=10;
+    static float b=20;
+
+    public static void main(String[] args) {
+        System.out.println("Addition of two numbers:"+(a+b));
+        System.out.println("subtraction of two numbers:"+(a-b));
+        System.out.println("multiplication of two numbers:"+(a*b));
+        System.out.println("Division of two numbers:"+(a/b));
+    }
+}
